dddfdea90dfe2222e1dc9b15f5bd572d964e0038
leechungyoon <36251628+LEECHUNGYOON@users.noreply.github.com>
2022-12-12 02:34:59 +0900

    update [2022-12-12 오전 2:33]
    # ws 설치시 아이콘에 보이는 이름 변경
    - 기존
    : u4a_ws3
    - 변경
    :U4A Workspace
    
    - UI5 라이브러리 탑재 버전 테스트

--- platforms/electron/www/ws10_20/Popups/releaseNotePopup/js/main_latest.js
+++ platforms/electron/www/ws10_20/Popups/releaseNotePopup/js/main_latest.js
@@ -11,7 +11,7 @@ function onLoadBootStrapSetting() {
         oSetting_UI5 = oSettings.UI5,
         sVersion = oSetting_UI5.version,
         sTestResource = oSetting_UI5.testResource,
-        sReleaseResource = `../../../../lib/ui5/${sVersion}/resources/sap-ui-core.js`,
+        sReleaseResource = `../../../lib/ui5/${sVersion}/resources/sap-ui-core.js`,
         bIsDev = oSettings.isDev,
         oBootStrap = oSetting_UI5.bootstrap,
         oUserInfo = oAPP.attr.oUserInfo,
