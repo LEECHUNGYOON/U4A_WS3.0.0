99d09047f6003db6b393500ccc3dd983209e0f13
LEECHUNGYOON <soccerhs@gmail.com>
2024-08-22 11:48:52 +0900

    팝업별 브로드 캐스트 작업완료!!
    팝업별 브로드 캐스트 작업완료!!

--- www/ws10_20/extopen.js
+++ www/ws10_20/extopen.js
@@ -16,9 +16,26 @@ var WSUTIL = require(WSMSGPATH);
 
 
 var oAPP = {};
+    oAPP.fn = {};
+    oAPP.attr = {};
+
+    oAPP.attr.isBusy = ""; // 현재 busy 상태
 
 
-function setBusy(bIsBusy) {
+/***********************************************************
+ * 현재 Busy의 상태를 구한다.
+ ***********************************************************/
+oAPP.fn.getBusy = function(){
+
+    return oAPP.attr.isBusy;
+
+}; // end of oAPP.fn.getBusy
+
+
+/***********************************************************
+ * 브라우저 처음 실행 시 보여지는 Busy Indicator
+ ***********************************************************/
+oAPP.fn.setBusyLoading = function(bIsBusy) {
 
     var oBusy = document.getElementById("u4a_main_load");
     if (!oBusy) {
@@ -34,41 +51,84 @@ function setBusy(bIsBusy) {
 
     oBusy.classList.add("u4a_loadersInactive");
 
-}
+}; // end of oAPP.fn.setBusyLoading
+
+/***********************************************************
+ * Busy 켜기 끄기
+ ***********************************************************/
+oAPP.fn.setBusyIndicator = function(bIsBusy, sOption){
+
+    oAPP.attr.isBusy = bIsBusy;
+
+    var _ISBROAD = sOption?.ISBROAD || undefined;
+
+    var oBusy = document.getElementById("u4aWsBusyIndicator");
+
+    if (!oBusy) {
+        return;
+    }
+
+    if (bIsBusy === "X") {
+
+        oBusy.style.visibility = "visible";
+
+        // 브라우저 창 닫기 버튼 비활성
+        CURRWIN.closable = false;
+
+        //다른 팝업의 BUSY ON 요청 처리.
+        if(typeof _ISBROAD === "undefined"){
+            oAPP.broadToChild.postMessage({PRCCD:"BUSY_ON"});
+        }      
+
+    } else {
+
+        oBusy.style.visibility = "hidden";
+
+        // 브라우저 창 닫기 버튼 활성
+        CURRWIN.closable = true;
+
+        //다른 팝업의 BUSY OFF 요청 처리.            
+        if(typeof _ISBROAD === "undefined"){
+            oAPP.broadToChild.postMessage({PRCCD:"BUSY_OFF"});
+        }
+
+    }
+
+}; // end of oAPP.fn.setBusyIndicator
 
 // 전달받은 html 경로를 Iframe의 경로로 변경한다.
 IPCRENDERER.on('if-extopen-url', (event, res) => {
 
     // BroadCast Event 걸기
-    _attachBroadCastEvent();
-
-    CURRWIN.show();
-
-    WSUTIL.setBrowserOpacity(CURRWIN);
-
-    // 화면이 다 그려지고 난 후 메인 영역 Busy 끄기
-    IPCRENDERER.send(`if-send-action-${BROWSKEY}`, { ACTCD: "SETBUSYLOCK", ISBUSY: "" }); 
-
-    IPCRENDERER.send(`if-send-action-${BROWSKEY}`, { ACTCD: "BROAD_BUSY", PRCCD: "BUSY_OFF" });
-
-    // 현재 윈도우의 닫기 버튼 활성화
-    CURRWIN.closable = true;
+    _attachBroadCastEvent();    
 
     var oFrame = document.getElementById("ws_exam");
     if (!oFrame) {
         return;
     }
-
-    setBusy(true);
+    
+    // 화면 처음 시작 시 실행되는 loadingbar 실행
+    oAPP.fn.setBusyLoading(true);
 
     oFrame.onload = function () {
 
-        oFrame.contentWindow.document.body.style.margin = "0px";
+        oFrame.contentWindow.document.body.style.margin = "0px";        
 
-        setBusy(false);
+        // 브라우저 활성화
+        CURRWIN.show();
+        
+        WSUTIL.setBrowserOpacity(CURRWIN);
 
         // 화면이 다 그려지고 난 후 메인 영역 Busy 끄기
-		IPCRENDERER.send(`if-send-action-${BROWSKEY}`, { ACTCD: "SETBUSYLOCK", ISBUSY: "" }); 
+        IPCRENDERER.send(`if-send-action-${BROWSKEY}`, { ACTCD: "SETBUSYLOCK", ISBUSY: "" }); 
+
+        IPCRENDERER.send(`if-send-action-${BROWSKEY}`, { ACTCD: "BROAD_BUSY", PRCCD: "BUSY_OFF" });
+
+        // 현재 윈도우의 닫기 버튼 활성화
+        CURRWIN.closable = true;
+
+        // 화면 처음 시작 시 실행되는 loadingbar 실행 종료
+        oAPP.fn.setBusyLoading(false);
 
     };
 
@@ -96,12 +156,13 @@ function _attachBroadCastEvent(){
             case "BUSY_ON":
 
                 //BUSY ON을 요청받은경우.
-                // oAPP.fn.setBusyIndicator("X", {ISBROAD:true});
+                oAPP.fn.setBusyIndicator("X", {ISBROAD:true});
                 break;
 
             case "BUSY_OFF":
+
                 //BUSY OFF를 요청 받은 경우.
-                // oAPP.fn.setBusyIndicator("",  {ISBROAD:true});
+                oAPP.fn.setBusyIndicator("",  {ISBROAD:true});
                 break;
 
             default:
@@ -146,3 +207,13 @@ window.addEventListener('message', function (e) {
     }
 
 });
+
+window.onbeforeunload = function(){
+
+    // 창 닫을 때 현재 Busy가 켜있다면 종료시키지 못하게 한다.
+    if(oAPP.fn.getBusy() === "X"){
+        return false;
+    }
+
+
+};
